b05ad90aee56cb5783c428dd7986e0f7b0ac80b4
janokom91 <alejandro.ortiz@kueski.com>
2022-04-05 06:40:51 +0000

    week 4

--- greenery/models/marts/Marketing/prod_funnel.sql
+++ greenery/models/marts/Marketing/prod_funnel.sql
@@ -1,2 +1,20 @@
-  - name: fact_user_orders
-    description: Contains detail of the orders placed by the users
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+select
+events.session_id,
+events.user_id,
+COALESCE(events.product_id, orderit.product_id) AS product_id,
+prod.product_name,
+prod.product_price,
+prod.inventory,
+{{col_values(ref('stg_events'),'event_type')}}
+FROM {{ ref('stg_events') }} events
+LEFT JOIN {{ ref('stg_order_items') }} orderit 
+ON events.order_id = orderit.order_id
+LEFT JOIN {{ ref('stg_products') }} prod 
+ON COALESCE(events.product_id, orderit.product_id) = prod.product_id
+{{ dbt_utils.group_by(n=6) }}
